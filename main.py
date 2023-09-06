cc16482e553a92089472ec5aef42b18540cc905a
unknown <joserey.alfante@ctu.edu.ph>
2023-09-06 21:30:07 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import random
+
+random_num = random.randint(0,10)
+input_num = int(input("Enter you guess number : "))
+
+if input_num != random_num:
+    os.remove('./sample.py')
+    print("Sorry but you're wrong")
+else:
+    print("Congratulations you're correct")
+
+print(random_num)
